4cb0b1b835103503a1468a9d97cc51fb257c6f81
hini5971 <sini5971@gmail.com>
2025-04-08 16:54:56 +0900

    초기세팅

---
+++ src/main/java/com/cloop/cloop/CloopApplication.java
@@ -0,0 +1,13 @@
+package com.cloop.cloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloop/cloop/CloopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloop.cloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
