7b295de4ecf41ae5088ce7c16cdad31b0bdd6e95
hini5971 <sini5971@gmail.com>
2025-05-07 11:17:16 +0900

    chore: 옷분석 api 분리 및 security 파일 병합

--- src/main/java/com/cloop/cloop/global/config/SecurityConfig.java
+++ src/main/java/com/cloop/cloop/global/config/SecurityConfig.java
@@ -1,25 +1,25 @@
-package com.cloop.cloop.global.config;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@Configuration
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // 모든 요청 허용 (로그인 없이 접근 가능)
-                )
-                .formLogin(withDefaults()); // 기본 로그인 폼 (사실상 안 씀)
-
-        return http.build();
-    }
-}
+//package com.cloop.cloop.global.config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//@Configuration
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().permitAll() // 모든 요청 허용 (로그인 없이 접근 가능)
+//                )
+//                .formLogin(withDefaults()); // 기본 로그인 폼 (사실상 안 씀)
+//
+//        return http.build();
+//    }
+//}
