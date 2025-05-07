0dccfe0c1e9ef3a2b1f16275789e87a3c66314cc
hini5971 <sini5971@gmail.com>
2025-05-07 12:40:55 +0900

    chore: SecurityConfig파일 수정

--- src/main/java/com/cloop/cloop/auth/config/SecurityConfig.java
+++ src/main/java/com/cloop/cloop/auth/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/google", "/auth/google/signup", "/auth/refresh", "/swagger-ui/**","/v3/api-docs/**", "/uploads/**").permitAll()
+                        .requestMatchers("/auth/google", "/auth/google/signup", "/auth/refresh", "/swagger-ui/**","/v3/api-docs/**", "/uploads/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 )
