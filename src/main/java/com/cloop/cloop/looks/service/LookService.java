6066ca0e2e763790adf14850ef7ff805b1fb622b
onlyoneprivate <pllm098@naver.com>
2025-05-08 14:41:10 +0900

    refactor: LookImage, LookService를 공통 image 기반 리팩토링

--- src/main/java/com/cloop/cloop/looks/service/LookService.java
+++ src/main/java/com/cloop/cloop/looks/service/LookService.java
@@ -1,9 +1,21 @@
 package com.cloop.cloop.looks.service;
 
-import com.cloop.cloop.looks.repository.LookImageRepository;
-import com.cloop.cloop.looks.repository.LookRepository;
-import org.springframework.transaction.annotation.Transactional;
+import com.cloop.cloop.global.file.ImageService;
+import com.cloop.cloop.looks.domain.Look;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class LookService {
+
+    private final ImageService imageService;
+
+    public void uploadLookImage(List<MultipartFile> imageList, Look look){
+        imageService.saveImage(imageList, look);
+    }
 
 }
