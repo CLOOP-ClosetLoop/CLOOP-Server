2eb66b3ab24984fd5a1be439ae677db3b08b9ff3
hini5971 <sini5971@gmail.com>
2025-05-11 18:07:31 +0900

    chore: donation관련 파일 추가

---
+++ src/main/java/com/cloop/cloop/looks/domain/LookCloth.java
@@ -0,0 +1,28 @@
+package com.cloop.cloop.looks.domain;
+
+import com.cloop.cloop.clothes.domain.Cloth;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LookCloth {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "look_id")
+    private Look look;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cloth_id")
+    private Cloth cloth;
+}
