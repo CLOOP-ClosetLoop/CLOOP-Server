dba4af1b48183d839564b3dd47e201e4c83fcd1a
hini5971 <sini5971@gmail.com>
2025-05-11 15:08:31 +0900

    chore: ClothResponse파일 작성

---
+++ src/main/java/com/cloop/cloop/clothes/dto/ClothResponse.java
@@ -0,0 +1,21 @@
+package com.cloop.cloop.clothes.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class ClothResponse {
+    private Long clothId;
+    private String clothName;
+    private String category;
+    private String brand;
+    private LocalDate purchasedAt;
+    private String color;
+    private String season;
+    private Boolean donated;
+    private String imageUrl;
+    private LocalDate lastWornAt;
+}
