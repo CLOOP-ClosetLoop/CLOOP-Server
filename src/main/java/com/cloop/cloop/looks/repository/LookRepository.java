72662878720e7eb920eb1ef3f7839865ce803844
onlyoneprivate <pllm098@naver.com>
2025-05-15 01:04:50 +0900

    feat: 날짜별 Look 조회 시 clothes 정보 즉시 로드

--- src/main/java/com/cloop/cloop/looks/repository/LookRepository.java
+++ src/main/java/com/cloop/cloop/looks/repository/LookRepository.java
@@ -1,6 +1,7 @@
 package com.cloop.cloop.looks.repository;
 
 import com.cloop.cloop.looks.domain.Look;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +12,10 @@ import java.util.List;
 public interface LookRepository extends JpaRepository<Look, Long> {
 
     // 날짜별 Look 조회
+    // Look -> LookCloth -> Cloth 즉시 로드
+    @EntityGraph(attributePaths = {"lookClothList.cloth"})
     @Query("SELECT l FROM Look l WHERE l.createdAt = :date")
     List<Look> findAllByCreatedAt(@Param("date") LocalDate date);
+
+
 }
